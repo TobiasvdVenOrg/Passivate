a93cd20ba02a211bf03946f330ee315caa4abcad
Tobias van de Ven <tobiasvdven@hotmail.com>
2024-10-12 12:33:31 +0200

    Request repaint on event

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,4 @@
 use std::process::Command;
-use std::time::Duration;
 use eframe::{Frame};
 use egui::Context;
 use futures::channel::mpsc::Receiver;
@@ -8,38 +7,37 @@ use notify::*;
 pub struct App {
     rx: Receiver<Result<Event>>,
     text: String,
-    cc: Context
+    cc: Context,
+    w: ReadDirectoryChangesWatcher
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
 
-        if let Ok(event) = self.rx.try_next() {
+        if let Ok(Some(event)) = self.rx.try_next() {
+            match event {
+                Ok(e) => {
+                    let path = std::env::args().nth(1).expect("Please supply a path to the directory of project's .toml file.");
+                    let output = Command::new("cargo").arg("test").current_dir(path).output().expect("Failed to run tests.");
 
-            println!("event: {:?}", event);
-            let path = std::env::args().nth(1).expect("Please supply a path to the directory of project's .toml file.");
-            let output = Command::new("cargo").arg("test").current_dir(path).output().expect("Failed to run tests.");
+                    let s = String::from_utf8_lossy(&output.stdout);
 
-            println!("Tests run!");
-            let s = String::from_utf8_lossy(&output.stdout);
-
-            self.text = s.to_string();
+                    self.text = s.to_string();
+                },
+                Err(err) => {
+                    println!("error: {:?}", err);
+                }
+            }
         }
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading(self.text.as_str());
         });
-
-        ctx.request_repaint_after(Duration::from_millis(100));
     }
 }
 
 impl App {
-    pub fn new(receiver: Receiver<Result<Event>>, context: Context) -> Self {
-        App { rx: receiver, text: "Hello!".to_string(), cc: context }
-    }
-
-    pub fn refresh(&self) {
-        self.cc.request_repaint()
+    pub fn new(receiver: Receiver<Result<Event>>, context: Context, watcher: ReadDirectoryChangesWatcher) -> Self {
+        App { rx: receiver, text: "Hello!".to_string(), cc: context, w: watcher }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@
 mod app;
 use notify::*;
 use std::path::Path;
+use eframe::CreationContext;
 use futures::channel::mpsc::channel;
 use futures::SinkExt;
 
-fn main() {
+fn build_app(cc: &CreationContext) -> Box<app::App> {
     let path = std::env::args().nth(1).expect("Please supply a path to the directory of project's .toml file.");
+    let (mut tx, rx) = channel(1);
 
-    let (mut tx, mut rx) = channel(1);
+    let cloned_context = cc.egui_ctx.clone();
     let mut watcher = RecommendedWatcher::new(move |res| {
         futures::executor::block_on(async {
             tx.send(res).await.unwrap();
+            cloned_context.request_repaint();
         })
     }, Config::default()).expect("Unable to create watcher.");
 
     let _ = watcher.watch(Path::new(&path), RecursiveMode::Recursive).expect("Unable to start watching.");
 
+    let mut app = Box::new(app::App::new(rx, cc.egui_ctx.clone(), watcher));
+
+    app
+}
+
+fn main() {
     let eframe_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
@@ -29,8 +38,8 @@ fn main() {
         "Passivate",
         eframe_options,
         Box::new(|cc| {
-            let app = app::App::new(rx, cc.egui_ctx.clone());
-            Ok(Box::new(app))
+            let app = build_app(cc);
+            Ok(app)
         }),
     ).expect("Unable to open window.");
 }
