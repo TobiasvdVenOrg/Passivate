cd506d010a2f3add1f6507abbda028f57f40d7ee
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-02-01 13:15:05 +0100

    Filter duplicate notify change events

--- passivate_core/src/passivate_notify.rs
+++ passivate_core/src/passivate_notify.rs
@@ -1,5 +1,11 @@
-use notify::{RecommendedWatcher, Config, RecursiveMode, Watcher};
-use std::path::Path;
+use std::collections::HashMap;
+use std::fs;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::Config as NotifyConfig;
+use notify::Event as NotifyEvent;
+use notify::Result as NotifyResult;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
 use crate::error::{PassivateError, Result};
 use crate::change_events::{ChangeEvent, ChangeEventHandler};
 
@@ -9,18 +15,52 @@ pub struct NotifyChangeEvents {
 
 impl NotifyChangeEvents {
     pub fn new(path: &str, mut handler: Box<dyn ChangeEventHandler>) -> Result<NotifyChangeEvents> {
-        let mut watcher = RecommendedWatcher::new(move |res| {
+        let mut config = NotifyConfig::default();
+        config.with_compare_contents(true);
+
+        let mut modification_cache: HashMap<PathBuf, SystemTime> = HashMap::new();
+
+        let mut watcher = RecommendedWatcher::new(move |res: NotifyResult<NotifyEvent>| {
             match res {
                 Ok(event) => {
-                    println!("{:?}", event);
-                    let change_event = ChangeEvent { };
-                    handler.handle_event(change_event);
+                    for path in &event.paths {
+                        let extension = path.extension();
+
+                        if let Some(extension) = extension {
+                            if extension == "rs" {
+                                println!("Checking...");
+                                if let Ok(metadata) = fs::metadata(path) {
+                                    if let Ok(modified) = metadata.modified() {
+                                        if let Some(last_modification) = modification_cache.get(path.as_path()) {
+                                            println!("Last modified was: {:?}", last_modification);
+
+                                            if &modified > last_modification {
+                                                println!("Running... {:?}", modified);
+                                                println!("{:?}", event);
+                                                let change_event = ChangeEvent { };
+                                                handler.handle_event(change_event);
+                                            } else {
+                                                println!("Ignoring...");
+                                            }
+                                        } else {
+                                            println!("Not in cache! {:?}", path);
+                                            let change_event = ChangeEvent { };
+                                            handler.handle_event(change_event);
+                                        }
+
+                                        modification_cache.insert(path.clone(), modified);
+                                    }
+                                }
+
+                            }
+                        }
+                    }
                 }
                 Err(error) => {
                     println!("{:?}", error);
                 }
             }
-        }, Config::default()).expect("Unable to create watcher.");
+        }, config).expect("Unable to create watcher.");
 
         let watch_result = watcher.watch(Path::new(&path), RecursiveMode::Recursive);
 
