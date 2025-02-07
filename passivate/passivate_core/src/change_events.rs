400034d2f224c5b1827f5c72b706bbb3d3e10781
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-02-08 00:37:57 +0100

    Restructuring main

--- passivate/passivate_core/src/change_events.rs
+++ passivate/passivate_core/src/change_events.rs
@@ -1,7 +1,5 @@
 pub mod change_event;
 pub mod handle_change_event;
-pub mod async_change_event_handler;
 
 pub use change_event::*;
 pub use handle_change_event::*;
-pub use async_change_event_handler::*;
--- passivate/src/main.rs
+++ passivate/src/main.rs
@@ -3,15 +3,35 @@ mod error_app;
 mod startup_errors;
 pub mod passivate_notify;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::SeqCst;
 use std::sync::mpsc::channel;
+use std::thread;
 use app::App;
-use eframe::CreationContext;
-use passivate_core::change_events::{AsyncChangeEventHandler, ChangeEvent};
+use passivate_core::change_events::{ChangeEvent, HandleChangeEvent};
 use passivate_notify::NotifyChangeEvents;
 use passivate_core::test_execution::{TestRunner};
 use crate::error_app::ErrorApp;
 use crate::startup_errors::*;
+
+fn main() {
+    match run_from_args() {
+        Ok(_) => {
+            println!("Exiting...");
+        }
+        Err(error) => {
+            run_app(ErrorApp::boxed(error));
+        }
+    }
+}
+
+fn run_from_args() -> Result<(), StartupError> {
+    let path = get_path_arg()?;
+
+    run_from_path(&path)
+}
 
 fn get_path_arg() -> Result<PathBuf, MissingArgumentError> {
     let path = std::env::args().nth(1);
@@ -21,9 +41,8 @@ fn get_path_arg() -> Result<PathBuf, MissingArgumentError> {
         None => Err(MissingArgumentError { argument: "path".to_string() })
     }
 }
-fn build_app(_cc: &CreationContext) -> Result<Box<dyn eframe::App>, StartupError> {
-    let path = get_path_arg()?;
 
+fn run_from_path(path: &Path) -> Result<(), StartupError> {
     let (change_event_sender, change_event_receiver) = channel();
 
     let _ = change_event_sender.send(ChangeEvent {});
@@ -31,13 +50,31 @@ fn build_app(_cc: &CreationContext) -> Result<Box<dyn eframe::App>, StartupError
     let change_events = NotifyChangeEvents::new(&path, change_event_sender)?;
 
     let (tests_status_sender, tests_status_receiver) = channel();
-    let test_execution = TestRunner::new(&path, tests_status_sender);
-    let _change_event_handler = AsyncChangeEventHandler::new(Box::new(test_execution), change_event_receiver);
+    let mut test_execution = TestRunner::new(&path, tests_status_sender);
 
-    Ok(App::boxed(tests_status_receiver, change_events))
+    let exit_flag: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+
+    let change_events_thread = thread::spawn({
+        let exit_flag = exit_flag.clone();
+        move || {
+            while !exit_flag.load(SeqCst) {
+                if let Ok(change_event) = change_event_receiver.recv() {
+                    test_execution.handle_event(change_event);
+                }
+            }
+        }
+    });
+
+    run_app(App::boxed(tests_status_receiver, change_events));
+
+    exit_flag.store(true, SeqCst);
+
+    change_events_thread.join().unwrap();
+
+    Ok(())
 }
 
-fn main() {
+fn run_app(app: Box<dyn eframe::App>) {
     let eframe_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
@@ -48,15 +85,8 @@ fn main() {
     eframe::run_native(
         "Passivate",
         eframe_options,
-        Box::new(|cc| {
-            let result = build_app(cc);
-
-            match result {
-                Ok(app) => Ok(app),
-                Err(error) => {
-                    Ok(ErrorApp::boxed(error))
-                }
-            }
+        Box::new(|_cc| {
+            Ok(app)
         }),
-    ).expect("failed to open window");
+    ).expect("Failed to start passivate!");
 }
