7be3c06ac379df6defe3ea1b20adda579ab1b1b8
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-02-28 23:28:21 +0100

    First POC snapshot view test

---
+++ passivate/passivate_app/tests/view_tests.rs
@@ -0,0 +1,23 @@
+use std::sync::mpsc::channel;
+
+use egui_kittest::Harness;
+use passivate::views::{TestsStatusView, View};
+use passivate_core::test_execution::TestsStatus;
+
+#[test]
+pub fn tests_status_view_shows_when_tests_are_running() {
+    let (sender, receiver)  = channel();
+    let mut tests_status_view = TestsStatusView::new(receiver);
+
+    let ui = |ui: &mut egui::Ui|{
+        tests_status_view.ui(ui);
+    };
+
+    let mut harness = Harness::new_ui(ui);
+
+    sender.send(TestsStatus::Running).unwrap();
+
+    harness.run();
+
+    harness.snapshot("tests_status_view_shows_when_tests_are_running");
+}
