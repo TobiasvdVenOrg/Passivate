7ce34fd408f3368a41b37b7c4721ff0761c01df2
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-03-08 20:50:05 +0100

    tests_status_view_shows_when_no_tests_were_found

--- passivate/passivate_app/src/tests/tests_status_view_tests.rs
+++ passivate/passivate_app/src/tests/tests_status_view_tests.rs
@@ -2,10 +2,22 @@ use std::sync::mpsc::channel;
 
 use egui_kittest::Harness;
 use crate::views::{TestsStatusView, View};
-use passivate_core::test_execution::TestsStatus;
+use passivate_core::test_execution::{CompleteTestsStatus, TestsStatus};
+use stdext::function_name;
 
 #[test]
 pub fn tests_status_view_shows_when_tests_are_running() {
+    run_and_snapshot(TestsStatus::Running, &test_name(function_name!()));
+}
+
+#[test]
+pub fn tests_status_view_shows_when_no_tests_were_found() {
+    let completed = CompleteTestsStatus { tests: vec!() };
+
+    run_and_snapshot(TestsStatus::Completed(completed), &test_name(function_name!()));
+}
+
+fn run_and_snapshot(tests_status: TestsStatus, snapshot_name: &str) {
     let (sender, receiver)  = channel();
     let mut tests_status_view = TestsStatusView::new(receiver);
 
@@ -15,9 +27,13 @@ pub fn tests_status_view_shows_when_tests_are_running() {
 
     let mut harness = Harness::new_ui(ui);
 
-    sender.send(TestsStatus::Running).unwrap();
+    sender.send(tests_status).unwrap();
 
     harness.run();
     harness.fit_contents();
-    harness.snapshot("tests_status_view_shows_when_tests_are_running");
+    harness.snapshot(snapshot_name);
+}
+
+fn test_name(function_name: &str) -> String {
+    function_name.split("::").last().unwrap_or(function_name).to_string()
 }
