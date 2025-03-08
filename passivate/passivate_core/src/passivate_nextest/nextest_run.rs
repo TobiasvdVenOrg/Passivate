ed89389135fe9b5d9beabf8e629cc7fe25af3634
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-03-08 22:48:05 +0100

    Fix real-time test run output forwarding to ui

--- passivate/passivate_core/src/passivate_nextest/nextest_run.rs
+++ passivate/passivate_core/src/passivate_nextest/nextest_run.rs
@@ -14,25 +14,10 @@ impl Nextest {
 impl RunTests for Nextest {
     fn run_tests(&mut self, sender: &Sender<TestsStatus>) -> Result<(), RunTestsError> {
         let _ = sender.send(TestsStatus::Running);
-        
+
         let output = self.test_run_command.spawn()?;
 
         let mut tests: Vec<SingleTest> = vec!();
-
-        if let Some(out) = output.stdout {
-            let reader = BufReader::new(out);
-
-            for line in reader.lines().map_while(Result::ok) {
-                let test = self.test_run_command.parser.parse_line(&line);
-
-                if let Some(test) = test {
-                    tests.push(test);
-
-                    let new_status = TestsStatus::completed(tests.clone());
-                    let _ = sender.send(new_status);
-                }
-            }
-        };
 
         if let Some(out) = output.stderr {
             let reader = BufReader::new(out);
