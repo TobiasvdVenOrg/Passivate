12ee5073dcbc5039545eb73d3a3d7ba30c344e96
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-03-13 20:19:38 +0100

    Deleting change_event_causes_test_run_and_results, too dependent on exact order of output

--- passivate/passivate_core/tests/cargo_test_tests.rs
+++ passivate/passivate_core/tests/cargo_test_tests.rs
@@ -19,6 +19,7 @@ pub fn tests_status_is_completed_after_run_with_no_tests_found(#[case] mut build
         .receive_tests_status(sender)
         .with_workspace("no_tests_project")
         .with_output(function_name!())
+        .clean_output()
         .build();
 
     test_run(&mut runner)?;
@@ -28,33 +29,6 @@ pub fn tests_status_is_completed_after_run_with_no_tests_found(#[case] mut build
 
     let completed = assert_matches!(completed, TestRun::Active);
     assert_eq!(0, completed.tests.len());
-
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
-#[rstest]
-#[case::cargo(cargo_builder())]
-#[case::nextest(nextest_builder())]
-pub fn change_event_causes_test_run_and_results(#[case] mut builder: TestRunnerBuilder) -> Result<(), IoError> {
-    let (sender, receiver) = channel();
-    let mut runner = builder
-        .receive_tests_status(sender)
-        .with_workspace("simple_project")
-        .with_output(function_name!())
-        .build();
-
-    test_run(&mut runner)?;
-
-    let starting = assert_matches!(receiver.try_recv().unwrap(), TestRun::Active);
-    assert_eq!(0, starting.tests.len());
-    
-    let _test1 = receiver.try_recv().unwrap();
-    let _test2 = receiver.try_recv().unwrap();
-    let test3 = receiver.try_recv().unwrap();
-
-    let completed = assert_matches!(test3, TestRun::Active);
-    assert_eq!(3, completed.tests.len());
 
     Ok(())
 }
