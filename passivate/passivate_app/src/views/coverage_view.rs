31437d4fae67a5d5a70f1bd44dc15c4f4cdde0a3
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-03-29 15:15:38 +0100

    Hide "expansion" button if coverage node has no children

--- passivate/passivate_app/src/views/coverage_view.rs
+++ passivate/passivate_app/src/views/coverage_view.rs
@@ -14,23 +14,30 @@ impl CoverageView {
         CoverageView { receiver, sender, status: CoverageStatus::Disabled }
     }
 
-    fn draw_coverage(ui: &mut egui_dock::egui::Ui, coverage: &CovdirJson, id: egui::Id) {  
-        let default_open = false;
-        CollapsingState::load_with_default_open(ui.ctx(), id, default_open)
-            .show_header(ui, |ui| {
+    fn draw_coverage(ui: &mut egui_dock::egui::Ui, coverage: &CovdirJson, id: egui::Id) {
+        if coverage.children.as_ref().is_none_or(|children|children.is_empty()) {
+            ui.horizontal(|ui| {
                 ui.label(&coverage.name);
                 ui.label(format!("{}%", &coverage.coverage_percent));
-            })
-            .body(|ui| {
-                if let Some(children) = &coverage.children {
-                    for child in children.values() {
-                        let hierarchical_id = egui::Id::new(format!("{:?}{}", id, child.name));
-                        Self::draw_coverage(ui, child, hierarchical_id);
-                    }
-                } else {
-                    ui.label("None");
-                }
             });
+        } else {
+            let default_open = false;
+            CollapsingState::load_with_default_open(ui.ctx(), id, default_open)
+                .show_header(ui, |ui| {
+                    ui.label(&coverage.name);
+                    ui.label(format!("{}%", &coverage.coverage_percent));
+                })
+                .body(|ui| {
+                    if let Some(children) = &coverage.children {
+                        for child in children.values() {
+                            let hierarchical_id = egui::Id::new(format!("{:?}{}", id, child.name));
+                            Self::draw_coverage(ui, child, hierarchical_id);
+                        }
+                    } else {
+                        ui.label("None");
+                    }
+                });
+        }
     }
 
     fn draw_disabled(&self, ui: &mut egui_dock::egui::Ui) {
