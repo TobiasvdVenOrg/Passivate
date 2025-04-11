3504a0f9fa25506e91cf4a5f7d34e23ba2cbdc0d
Tobias van de Ven <tobiasvdven@hotmail.com>
2025-04-11 20:28:42 +0200

    Fine, default Default

--- passivate/passivate_core/src/configuration/passivate_config.rs
+++ passivate/passivate_core/src/configuration/passivate_config.rs
@@ -1,16 +1,7 @@
 
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct PassivateConfig {
     pub coverage_enabled: bool,
     pub snapshots_path: Option<String>
-}
-
-impl Default for PassivateConfig {
-    fn default() -> Self {
-        Self { 
-            coverage_enabled: false,
-            snapshots_path: None 
-        }
-    }
 }
